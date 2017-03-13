88d4adff0506b4a3f4e6bb55f2411d014fc5c0da
joao <joao@Desktop-PC>
2017-03-12 22:03:05 -0300

    Primeira Versão 1.1 (Sistema Help Desk)

---
+++ CursoJava/src/main/java/br/edu/ifrn/MyApplication.java
@@ -0,0 +1,14 @@
+package br.edu.ifrn;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("rest")
+public class MyApplication extends ResourceConfig {
+
+    public MyApplication() {
+        packages("br.edu.ifrn.controllers");
+    }
+     
+    
+}
